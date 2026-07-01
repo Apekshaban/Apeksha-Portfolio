f5d145dd929ed07bc6ff2babb25535e8888df71e
Apeksha-1-hub <meeraban123@gmail.com>
2026-07-01 12:08:44 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="app">
+      <h1>React + JavaScript + Tailwind Ready</h1>
+      <p>
+        This project now uses JavaScript instead of TypeScript.
+      </p>
+    </main>
+  )
+}
+
+export default App
